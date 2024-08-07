21cd6a9747ed061aa336143afa80f730730f9b53
Gee <hong.geewoon@gmail.com>
2024-08-07 18:46:26 +0900

    Finished Remove Dup

--- Solutions/RemoveDuplicatesArr.c
+++ Solutions/RemoveDuplicatesArr.c
@@ -1,14 +1,57 @@
-int removeElement(int* nums, int numsSize, int val) {
-    int i = 1;
+/*
+Given an integer array nums sorted in non-decreasing order,
+remove the duplicates in-place such that each unique element
+appears only once. The relative order of the elements should
+be kept the same. Then return the number of unique elements in nums.
+*/
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize)
+{
+    int i = 0;
     int k = 0;
     while (i < numsSize)
     {
-        if (nums[i] != nums[i-1])
+        if (((i+1) < numsSize) && (nums[i] != nums[i+1]))
         {
             nums[k] = nums[i];
             k++;
         }
-        i++;
+		else if((i+1) == numsSize)
+        {
+			nums[k] = nums[i];
+			k++;
+        }
+		i++;
     }
     return (k);
+}
+
+void	printArray(int *array, int size)
+{
+	int i = 0;
+	while (i < size)
+	{
+		printf("%d ", array[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int tst1[] = {1,1,2};
+	int tst2[] = {0,0,1,1,1,2,2,3,3,4};
+
+	int size1 = sizeof(tst1) / sizeof(tst1[0]);
+	int size2 = sizeof(tst2) / sizeof(tst2[0]);
+
+	int newsize1 = removeDuplicates(tst1, size1);
+	int newsize2 = removeDuplicates(tst2, size2);
+
+	printf("After modification for tst1: ");
+	printArray(tst1, newsize1);
+	printf("After modification for tst2: ");
+	printArray(tst2, newsize2);
+	return (0);
 }
--- Solutions/RemoveElement.c
+++ Solutions/RemoveElement.c
@@ -6,6 +6,7 @@ Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 */
+#include <stdio.h>
 
 int removeElement(int* nums, int numsSize, int val) {
     int i = 0;
@@ -20,4 +21,38 @@ int removeElement(int* nums, int numsSize, int val) {
         i++;
     }
     return (k);
+}
+
+void	printArray(int *array, int size)
+{
+	int i = 0;
+	while (i < size)
+	{
+		printf("%d ", array[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+int	main()
+{
+	int tst1[] = {3,2,2,3};
+	int tst2[] = {1,2,2,3,5,3,2};
+	int tst3[] = {1,2,2,2,2,1,2,1,1,2,1,1,4,5,6,8,2,10,2};
+
+	int size1 = sizeof(tst1) / sizeof(tst1[0]);
+	int size2 = sizeof(tst2) / sizeof(tst2[0]);
+	int size3 = sizeof(tst3) / sizeof(tst3[0]);
+
+	int newsize1 = removeElement(tst1, size1, 3);
+	int newsize2 = removeElement(tst2, size2, 3);
+	int newsize3 = removeElement(tst3, size3, 2);
+
+	printf("After modification for tst1: ");
+	printArray(tst1, newsize1);
+	printf("After modification for tst2: ");
+	printArray(tst2, newsize2);
+	printf("After modification for tst3: ");
+	printArray(tst3, newsize3);
+	return (0);
 }
